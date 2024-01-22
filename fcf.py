b8832dcdb5e37a0f47d9f0a9d2589c389ffaa610
DanielG0m <danicam465@gmail.com>
2024-01-21 19:15:02 -0500

    Primera edicion

--- fcf.py
+++ fcf.py
@@ -13,7 +13,6 @@ titulo = """
 
 opciones = "1. Registra un equipo \n2. Registra los partidos \n3. Reporte\n4. Salir"
 tg=0
-tp=0
 equipos = []
 pj=0 
 pg=0
@@ -38,7 +37,7 @@ while True:
                 ex=True
                 break
         if ex:
-            print("El equipo existe")
+            print("El equipo ya esta registrado. ")
             os.system('pause')
         else:
             id = str(len(equipos)).zfill(0)
@@ -46,7 +45,7 @@ while True:
             os.system('pause')
 
     elif op == 2:
-        l = input("Ingrese el equipo de local   : ")
+        l = input("Ingrese el equipo que jugo de local: ")
         lexist=False
         for item in equipos:
              if l in item[0]:
@@ -56,7 +55,7 @@ while True:
              print("El equipo no ha sido registrado ")
              os.system('pause')
         else:
-            v = input("Ingrese el equipo de visitante  : ")
+            v = input("Ingrese el equipo que jugo de visitante: ")
             vexist=False
             for item in equipos:
                  if v in item[0]:
@@ -69,44 +68,48 @@ while True:
                  print("El equipo no puede jugar contra si mismo")
                  os.system('pause')
             else:
-                marcadorlocal = int(input("Ingrese el marcador del equipo local : "))
+                marcadorlocal = int(input("Ingrese la cantidad de goles del equipo local: "))
                 if marcadorlocal<0:
                     print("No puedes ingresar valores negativos")
                     os.system('pause')
                 else:    
-                    marcadorvisitante = int(input("Ingrese el marcador del equipo visitante : "))
+                    marcadorvisitante = int(input("Ingrese la cantidad de goles del equipo visitante: "))
                     if marcadorvisitante<0:
                         print("No puedes ingresar valores negativos")
                         os.system('pause')
                     else:
                         tg += marcadorlocal + marcadorvisitante
                         tp += 2
-                        if marcadorlocal>=0 and marcadorvisitante>=0:
-                            for i, item in enumerate (equipos):
-                                if l in item:
-                                    equipos[i][1] +=1
-                                    equipos[i][5] += marcadorlocal
-                                    equipos[i][6] += marcadorvisitante
-                                    equipos[i][7] += 1
-                                    if marcadorlocal > marcadorvisitante:
-                                        equipos[i][2] += 1
-                                    elif marcadorlocal < marcadorvisitante:
-                                        equipos[i][3] += 1
-                                    elif marcadorlocal == marcadorvisitante:
-                                        equipos[i][4] +=1
+                        for i,item in enumerate (equipos):
+                            if(l in item):
+                                equipos[i][1] += 1
+                                equipos[i][5] += marcadorlocal
+                                equipos[i][6] += marcadorvisitante
 
-                                elif v in item:
-                                    equipos[i][1] +=1
-                                    equipos[i][5] += marcadorvisitante
-                                    equipos[i][6] += marcadorlocal
+                            elif(v in item):
+                                equipos[i][1] += 1
+                                equipos[i][5] += marcadorvisitante
+                                equipos[i][6] += marcadorlocal
+
+                        for i,item in enumerate (equipos):
+                            if(l in item):
+                                if(marcadorlocal > marcadorvisitante):
+                                    equipos[i][2] += 1
+                                    equipos[i][7] += 3
+                                elif(marcadorlocal == marcadorvisitante):
+                                    equipos[i][4] += 1
                                     equipos[i][7] += 1
-                                if marcadorlocal < marcadorvisitante:
-                                        equipos[i][2] += 1
-                                elif marcadorlocal > marcadorvisitante:
-                                        equipos[i][3] += 1
-                                elif marcadorlocal == marcadorvisitante:
-                                        equipos[i][4] +=1
-                                
+                                else:
+                                    equipos[i][3] += 1
+                            elif(v in item):
+                                if(marcadorvisitante > marcadorlocal):
+                                    equipos[i][2] += 1
+                                    equipos[i][7] += 3
+                                elif(marcadorlocal == marcadorvisitante):
+                                    equipos[i][4] += 1
+                                    equipos[i][7] += 1
+                                else:
+                                    equipos[i][3] += 1
     elif op == 3:
         tabla_de_puntajes = print("Reporte FCF")
         print(tabulate(equipos,headers=["Equipo","PJ","PG","PP","PE","GF","GC","TP"]))
@@ -115,23 +118,26 @@ while True:
         maxgoles = max(equipos, key=lambda n:n[5])
         equipos_maxgoles = [equipo[0] for equipo in equipos if equipo[5] == maxgoles[5]]
         if len(equipos_maxgoles) > 1:
-            print("Los equipos con más goles son:", equipos_maxgoles)
+            print("Los equipos con mas goles:", equipos_maxgoles)
         else:
-            print("El equipo con más goles es: ", equipos_maxgoles[0])
+            print("El equipo que hizo mas goles: ", equipos_maxgoles[0])
             maxpuntos= max(equipos, key=lambda n:n[7])
-            print("El equipo con mas puntos es: ",maxpuntos[0])
+            print("El equipo que hizo mas puntos: ",maxpuntos[0])
         print("")
 
         maxvictorias=max(equipos, key=lambda n:n[2])
         equipos_maxvictorias = [equipo[0] for equipo in equipos if equipo[2] == maxvictorias[2]]
         if len(equipos_maxvictorias) > 1:
-            print("Los equipos con mas victorias son:", equipos_maxvictorias)
+            print("Los equipos con mas victorias:", equipos_maxvictorias)
         else:
-            print("El equipo con mas victorias es: ", equipos_maxvictorias[0])
+            print("El equipo con mas victorias: ", equipos_maxvictorias[0])
         print("")
 
-        print("El total de goles en el torneo es: ",tg)
-        print("El promedio de goles es:  ",float(tg/tp))
+        print("Total de goles anotados en el torneo: ",tg)
+
+        print("")
+        print("Promedio de cantidad de goles:  ",float(tg/tp))
+        print("")
         os.system("pause")
 
     elif op == 4:
